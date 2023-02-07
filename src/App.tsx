a719aea1500cfaa3361cd151df853c18aebb5e95
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-02-07 14:33:14 +0530

    project setup and basic structure

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Footer from "./core/Footer";
+import Header from "./core/Header";
+
+function App() {
+  return (
+    <div className="h-100 d-flex flex-column">
+      <header id="header">
+        <Header />
+      </header>
+      <main className="main-content flex-grow-1"></main>
+      <footer id="footer">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default App;
