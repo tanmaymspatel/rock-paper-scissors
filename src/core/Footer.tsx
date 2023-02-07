7a1db30566bd8e601936826492fe8950fbdf71dc
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-02-07 16:18:53 +0530

    feat: footer design completed

--- src/core/Footer.tsx
+++ src/core/Footer.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import RulesOverlay from "./RulesOverlay";
+
 function Footer() {
+    const [showOverlay, setShowOverlay] = useState<boolean>(false)
     return (
-        <div>
-            Footer
-        </div>
+        <>
+            {showOverlay &&
+                <RulesOverlay
+                    setShowOverlay={setShowOverlay}
+                />}
+            <div className="container h-100">
+                <div className="footer-content">
+                    <button className="btn btn-outline-light text-light" onClick={() => setShowOverlay(true)}>
+                        <span className="btn-text">
+                            RULES
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </>
     )
 };
 
