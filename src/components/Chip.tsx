778e0251c7ada4c0b81ca4d73a09d2e28f093c06
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-02-09 12:32:03 +0530

    feat: addition of box shadow in winning side

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -2,14 +2,15 @@ import { useContext } from "react";
 import { GameContext } from "../context/game-context/gameContext";
 
 interface IChipProps {
-    name: string;
+    name: string,
+    win?: string
 }
-function Chip({ name }: IChipProps) {
+function Chip({ name, win }: IChipProps) {
 
     const { playing, chipClickHandler } = useContext(GameContext);
 
     return (
-        <div className={`chip-${!name ? "default" : name} chip-container ${!playing ? "position-absolute chip-hover cursor-pointer chip-before" : "position-relative chip-after pointer-events-none"} d-flex align-items-center justify-content-center border-circle `}
+        <div className={`chip-${!name ? "default" : name} ${win} ${!playing ? "position-absolute chip-hover cursor-pointer chip-before" : "position-relative chip-after pointer-events-none"} chip-container d-flex align-items-center justify-content-center border-circle`}
             onClick={() => chipClickHandler(name)}
         >
             <div className="inner-circle d-flex align-items-center justify-content-center border-circle">
--- src/components/GamePlay.tsx
+++ src/components/GamePlay.tsx
@@ -5,17 +5,17 @@ import Chip from "./Chip";
 import Result from "./Result";
 
 function GamePlay() {
-    const { picked, housePicked, gameOver } = useContext(GameContext);
+    const { picked, housePicked, gameOver, result } = useContext(GameContext);
 
     return (
         <div className={`${gameOver ? "game-over" : ""} game-play-container h-100 w-100 d-flex justify-content-between position-relative`}>
             <div className="d-flex flex-column align-items-center">
                 <h4 className="game-title text-light">You Picked</h4>
-                <Chip name={picked} />
+                <Chip name={picked} win={result === 'win' ? "win" : ""} />
             </div>
             <div className="d-flex flex-column align-items-center">
                 <h4 className="game-title text-light">The House Picked</h4>
-                <Chip name={housePicked} />
+                <Chip name={housePicked} win={result === 'lose' ? "win" : ""} />
             </div>
             <Result />
         </div>
